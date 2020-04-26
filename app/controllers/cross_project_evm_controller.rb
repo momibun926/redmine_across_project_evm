f5250fef1236bf812cff26362bd111ba6cebb57a
Hajime Nakagama <hajime.nakagama@gmail.com>
2020-04-26 12:17:04 +0900

    initial

--- app/controllers/cross_project_evm_controller.rb
+++ app/controllers/cross_project_evm_controller.rb
@@ -1,25 +1,18 @@
-# EVM controller.
-# This controller provide main evm view.
-#
-# 1. before action (override)
-# 2. selectable list for baseline
-# 3. calculate EVM all projects include desendant
-# 4. incomplete issues
-# 5. export to CSV
-#
 class CrossProjectEvmController < ApplicationController
   include IssueDataFetcher2
   include CalculateEvmLogic2
 
   def index
     @projects_evm = {}
-    selected_projects = Project.all
-    Rails.logger.info("selected_projects: #{selected_projects}")
-    selected_projects.each do |proj|
-      issues = evm_issues proj.id
-      issues_costs = evm_costs proj.id
-      @projects_evm[proj.id] = CalculateEvm2.new Time.current.to_date, issues, issues_costs
-      @projects_evm[proj.id].description = proj.name
+    selected_projects = Project.where(status: 1).
+                          where("members.user_id = ?", User.current.id).
+                          joins(:members).
+                          pluck(:id, :name)
+    selected_projects.each do |proj_id, proj_name|
+      issues = evm_issues proj_id
+      issues_costs = evm_costs proj_id
+      @projects_evm[proj_id] = CalculateEvm2.new Time.current.to_date, issues, issues_costs
+      @projects_evm[proj_id].description = proj_name
     end
   end
 end
--- lib/issue_data_fetcher2.rb
+++ lib/issue_data_fetcher2.rb
@@ -1,12 +1,6 @@
-# Issue data fetcher
-# This module is a function to collect ISSUE records necessary to calculate EVM
-# It also collects a selectable list that is optionally specified
-#
 module IssueDataFetcher2
   # Common condition of issue's select
   SQL_COM = <<-SQL_COM.freeze
-  (issues.project_id = ?)
-  AND
   (issues.start_date IS NOT NULL AND issues.due_date IS NOT NULL)
   OR
   (issues.start_date IS NOT NULL
@@ -16,40 +10,18 @@ module IssueDataFetcher2
    issues.fixed_version_id IN (SELECT id FROM versions WHERE effective_date IS NOT NULL))
   SQL_COM
 
-  # Common condition of issue's join for parent issue
-  SQL_JOIN = <<-"SQL_JOIN".freeze
-  JOIN #{Issue.table_name} ancestors
-    ON ancestors.root_id = #{Issue.table_name}.root_id
-    AND ancestors.lft <= #{Issue.table_name}.lft
-    AND ancestors.rgt >= #{Issue.table_name}.rgt
-  SQL_JOIN
-
-  # Get issues of EVM for PV and EV.
-  # Include descendants project. Rerequires inputted start date and due date.
-  # for use calculate PV and EV.
-  #
-  # @param [numeric] project_id project object
-  # @return [Issue] issue object
   def evm_issues(project_id)
-    Issue.where(SQL_COM.to_s, project_id)
+    Issue.where(project_id: project_id).
+      where(SQL_COM.to_s)
   end
 
-  # Get spent time of project.
-  # Include descendants project.require inputted start date and due date.
-  #
-  # @param [numeric] project_id project
-  # @return [hash] Two column,spent_on,sum of hours
   def evm_costs(project_id)
-    Issue.where(SQL_COM.to_s, project_id).
+    Issue.where(project_id: project_id).
+      where(SQL_COM.to_s).
       joins(:time_entries).
       group(:spent_on).sum(:hours)
   end
 
-  # latest setted done ratio journal
-  #
-  # @param [issue] issue issue record
-  # @param [date] basis_date basis date
-  # @return [journal] first of journals
   def issue_journal(issue, basis_date)
     Journal.where(journalized_id: issue.id, journal_details: { prop_key: "done_ratio" }).
       where("created_on <= ?", basis_date.end_of_day).
@@ -57,10 +29,6 @@ module IssueDataFetcher2
       order(created_on: :DESC).first
   end
 
-  # children of parent isuue
-  #
-  # @param [issue] issue parent issue
-  # @return [issue] cildren issue
   def issue_child(issue)
     Issue.where(root_id: issue.root_id).
       where("lft > ? AND rgt < ?", issue.lft, issue.rgt).
