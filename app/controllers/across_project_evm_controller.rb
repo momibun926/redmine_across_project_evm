127930a12fdfc3c4718dcf1731b4d182a0294038
Hajime Nakagama <hajime.nakagama@gmail.com>
2020-04-30 03:07:22 +0900

    Add csv download

--- app/controllers/across_project_evm_controller.rb
+++ app/controllers/across_project_evm_controller.rb
@@ -1,14 +1,24 @@
 require "evm_creator"
+require "csv_data_creator"
 
 # controller
 class AcrossProjectEvmController < ApplicationController
   include EvmCreator
+  include CsvDataCreator
 
   def index
     @basis_date = default_basis_date
     @working_hours_per_day = default_work_hour
     @selected_status = params[:selected_status] || Project::STATUS_ACTIVE
     @projects_evm = create_project_evm @basis_date, @selected_status
+    respond_to do |format|
+      format.html
+      format.csv do
+        send_data create_csv(@basis_date, @projects_evm, @working_hours_per_day),
+                  type: "text/csv; header=present",
+                  filename: "evm_#{@basis_date}.csv"
+      end
+    end
   end
 
   private
